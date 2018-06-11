5f7283f73296397a9b6c135c747cf24854edde24
wkdewey@gmail.com <wkdewey@gmail.com>
2018-06-11 00:54:27 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,9 +15,9 @@ function frameIt() {
 function pressIt() {
   $('form:input').on("keydown", function(key) {
     if(key.which === 71) {
-      
+      alert("You pressed the G key");
     }
-  })
+  });
 }
 $(document).ready(function(){
 
