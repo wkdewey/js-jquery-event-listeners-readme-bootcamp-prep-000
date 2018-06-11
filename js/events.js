4554e6e43c87d223be9ac33df0887d0294105f69
wkdewey@gmail.com <wkdewey@gmail.com>
2018-06-11 01:06:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,30 @@
 //define functions here
+function getIt() {
+  $('p').on("click",
+  function() {
+    alert("Hey!");
+  });
+}
 
+function frameIt() {
+  $('img').on("load", function(image) {
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $(':text').on("keydown", function(key) {
+    if(key.which === 71) {
+      alert("You pressed the G key");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on("submit", function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
 
 // call functions here
