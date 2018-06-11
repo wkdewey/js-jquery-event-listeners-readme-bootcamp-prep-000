c595fb4aad8824436ba71e777703042b7d4417ad
wkdewey@gmail.com <wkdewey@gmail.com>
2018-06-11 00:45:00 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,20 @@
 //define functions here
+function getIt() {
+  $('p').on("click",
+  function() {
+    alert("Hey!");
+  });
+}
 
+function frameIt() {
+  $('img').on("load", function(image) {
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt() {
+  $()
+}
 $(document).ready(function(){
 
 // call functions here
